d83d6f4a7fe16512d08b55bb56a7bd8c6e9cf446
Strange-Philip <philipabakahmensah@gmail.com>
2021-06-30 00:11:25 -0700

    skeleton done

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:indie/speechapi.dart';
+import 'package:indie/substring.dart';
+import 'package:indie/utils.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +11,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String text = 'Press the button and start speaking';
+  bool isListening = false;
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(""),
+          centerTitle: true,
+          actions: [
+            Builder(
+              builder: (context) => IconButton(
+                icon: Icon(Icons.content_copy),
+                onPressed: () async {
+                  await FlutterClipboard.copy(text);
+
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(content: Text('✓   Copied to Clipboard')),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          reverse: true,
+          padding: const EdgeInsets.all(30).copyWith(bottom: 150),
+          child: SubstringHighlight(
+            text: text,
+            terms: Command.all,
+            textStyle: TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+            textStyleHighlight: TextStyle(
+              fontSize: 32.0,
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: AvatarGlow(
+          animate: isListening,
+          endRadius: 75,
+          glowColor: Theme.of(context).primaryColor,
+          child: FloatingActionButton(
+            child: Icon(isListening ? Icons.mic : Icons.mic_none, size: 36),
+            onPressed: toggleRecording,
+          ),
+        ),
+      );
+
+  Future toggleRecording() => SpeechApi.toggleRecording(
+        onResult: (text) => setState(() => this.text = text),
+        onListening: (isListening) {
+          setState(() => this.isListening = isListening);
+
+          if (!isListening) {
+            Future.delayed(Duration(seconds: 1), () {
+              Utils.scanText(text);
+            });
+          }
+        },
+      );
 }
